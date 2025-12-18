0a5a95bb50263cf4b9eee027aa4cecb15caaaba5
Mark Dierolf <mdierolf@cloudquote.io>
2025-12-18 13:55:42 -0800

    Updated files to use `satisfies` for type assertion

--- commands/webhost.ts
+++ commands/webhost.ts
@@ -50,4 +50,4 @@ export default {
   description,
   execute,
   help,
-} as TokenRingAgentCommand;
+} satisfies TokenRingAgentCommand;
--- plugin.ts
+++ plugin.ts
@@ -34,4 +34,4 @@ export default {
       }
     }
   }
-} as TokenRingPlugin;
+} satisfies TokenRingPlugin;
